17bbeb13d8c88a3b43801f9f161689b239bb3736
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2015-08-26 15:32:34 +1000

    Build 0.1
    This has the basic setup of the website, with issues in Stories
    Controller

---
+++ GreenRevolution/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreenRevolution.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "We care about you more than you think.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Green Revolution contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ GreenRevolution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GreenRevolution.MvcApplication" Language="C#" %>
